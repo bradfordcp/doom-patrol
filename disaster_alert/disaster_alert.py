164452cedd6880fd913e8042ff29e1767a98ec64
mando222 <evemsmando@gmail.com>
2020-09-11 15:24:44 -0500

    built out the desaster app a bit

--- disaster_alert/disaster_alert.py
+++ disaster_alert/disaster_alert.py
@@ -4,6 +4,7 @@ import time
 import yaml
 import json
 from geopy.geocoders import Nominatim
+from pprint import pprint
 
 with open(r'config.yaml') as file:
     # The FullLoader parameter handles the conversion from YAML
@@ -11,10 +12,9 @@ with open(r'config.yaml') as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
 
 def parse_event_type(response):
-    response = json.loads(response.text)
-    data = response['data'][0]['features'][0]['properties']['title']
-    return data
-
+        response = json.loads(response.text)
+        data = response['data'][0]['features'][0]['properties']['title']
+        return data
 
 while True:
     #select favorate locations from the database
@@ -22,14 +22,18 @@ while True:
     #Ask database if there were any desasters at locations
     # for location in locations:
     # rest call to return any active event at *location*, and time now()
-    # if event found then have carter tell us
+    response = requests.get(config['server_ip']+"spoof_get_events.json/")
+    statuscode = response.status_code
+    print(statuscode)
 
-    try:
-        response = requests.get(config['server_ip']+"spoof_get_events.json/")
+    # if event found then have carter tell us
+    if statuscode == 200:
         event_type = parse_event_type(response)
         carter.say(f"Disaster  {event_type} detected near PLACEHOLDER")
-    except requests.exceptions.RequestException as e:  # This is the correct syntax
-        print(e)
+    else:
+        msg = f'error {statuscode} talking to jackson server'
+        print(msg)
+        carter.say(msg)
 
 
 
