5c3b2e037234c64d9e13f02d27c136b3cf0fb251
Matt Overstreet <matt.overstreet@gmail.com>
2020-09-11 21:21:47 +0000

    geo api ready to roll out (slowly)!

--- backend/geo.py
+++ backend/geo.py
@@ -8,7 +8,7 @@ class Geo:
   __kastra = AstraClient.new().keyspaces()
   def __init__(self, geo):
     poly = shapely.geometry.shape(geo)
-    self.hashes = polygon_to_geohashes(poly, 7, inner=False)
+    self.hashes = polygon_to_geohashes(poly, 5, inner=False)
     self.geo = geo
 
   def save(self):
@@ -22,14 +22,22 @@ class Geo:
     # TODO get to an async verion of this
     #result = map(lambda x: {'geohash': x, 'eid': id}, self.hashes)
     #self.__astra.save_all("geo7", result)
-
-
-    #print(list(result)) 
     return id
 
-  def intersects_with(geo):
+  def intersects_with(self):
     # break down hashes for geo and turn them into queries
-    ids = __astra.find_all("geo7", geo.hashes)
+
+    ids = set()
+    for hash in self.hashes:
+      result = self.__kastra.query_pk("geohash", [hash])
+      for d in result['data']:
+        ids.add(d['id'])
+    
+    #TODO get an async version of this running
+    #ids = __astra.find_all("geo7", geo.hashes)
+    results = []
+    for id in ids:
+      results.append(self.__astra.get("events", id))
 
     # return all documents that match unique ids from queries
-    return __astra.get_docs("events", ids)
+    return results
