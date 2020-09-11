0a8c6056b9fac606c7398294433c2df2f4797056
Christopher Bradford <bradfordcp@gmail.com>
2020-09-11 17:38:25 +0000

    Refactor Astra client. See scratch.py for example

--- backend/astra.py
+++ backend/astra.py
@@ -4,32 +4,47 @@ import uuid
 from datetime import datetime,timezone
 import json
 
-class Astra:
+class AstraClient:
     __REFRESH_INTERVAL = 1800
 
-    def __init__(self, database_id, region, username, password, keyspace):
-        self.database_id = database_id
-        self.region = region
-
-        self.__API_ROOT = f"https://{self.database_id}-{self.region}.apps.astra.datastax.com/api/rest"
+    __instance = None
+    def __new__(cls, database_id, region, username, password):
+        if AstraClient.__instance is None:
+            AstraClient.__instance = object.__new__(cls)
         
-        self.username = username
-        self.password = password
-        self.keyspace = keyspace
+        AstraClient.__instance.database_id = database_id
+        AstraClient.__instance.region = region
+        AstraClient.__instance.username = username
+        AstraClient.__instance.password = password
+        AstraClient.__instance.__token = None
+        AstraClient.__instance.__token_refreshed_at = None
+        AstraClient.__instance.__API_ROOT = f"https://{database_id}-{region}.apps.astra.datastax.com/api/rest"
 
-        self.__token = None
-        self.__token_refreshed_at = None
+        return AstraClient.__instance
     
+    def from_environment():
+        db = os.environ['ASTRA_DATABASE_ID']
+        region = os.environ['ASTRA_REGION']
+        username = os.environ['ASTRA_DATABASE_USERNAME']
+        password = os.environ['ASTRA_DATABASE_PASSWORD']
+
+        client = AstraClient(db, region, username, password)
+
+        if 'ASTRA_KEYSPACE' in os.environ:
+            return client.documents(os.environ['ASTRA_KEYSPACE'])
+        else:
+            return client
+    
+    def documents(self, keyspace):
+        return AstraDocuments(self, keyspace)
+
     def __needs_refresh(self):
         return self.__token_refreshed_at == None or (datetime.now(timezone.utc) - self.__token_refreshed_at).seconds > __REFRESH_INTERVAL
     
     def __refresh_token(self):
         if  self.__needs_refresh():
-            url = f"{self.__API_ROOT}/v1/auth"
-            headers = {
-                'X-Cassandra-Request-Id': uuid.uuid4().hex,
-                'Content-Type': "application/json"
-            }
+            url = f"{self.__api_root()}/v1/auth"
+            headers = self.__unauthenticated_headers()
             body = {
                 'username': self.username,
                 'password': self.password
@@ -41,19 +56,62 @@ class Astra:
                 auth_info = resp.json()
                 self.__token = auth_info['authToken']
                 self.__token_refreshed_at = datetime.now(timezone.utc)
+            else:
+                raise RuntimeError("Could not create token")
     
-    def create_document(self, collection, document=dict(), id=None):
-        self.__refresh_token()
-        
-        url = f"{self.__API_ROOT}/v2/namespaces/{self.keyspace}/collections/{collection}"
-        headers = {
+    def __unauthenticated_headers(self, existing={}):
+        existing.update({
             'X-Cassandra-Request-Id': uuid.uuid4().hex,
+            'Content-Type': 'application/json'
+        })
+        return existing
+
+    def __authenticated_headers(self, existing={}):
+        self.__refresh_token()
+        existing.update(self.__unauthenticated_headers())
+        existing.update({
             'X-Cassandra-Token': self.__token,
-            'Content-Type': "application/json"
-        }
-        
-        resp = requests.post(url, headers=headers, data=json.dumps(document))
+        })
+        return existing
+
+    def __api_root(self):
+        return f"https://{self.database_id}-{self.region}.apps.astra.datastax.com/api/rest"
+    
+    def get(self, path="", headers={}, **kwargs):
+        url = f"{self.__api_root()}{path}"
+        headers = self.__authenticated_headers(headers)
+        return requests.get(url, headers=headers, **kwargs)
+
+    def post(self, path="", headers={}, **kwargs):
+        print(kwargs)
+        url = f"{self.__api_root()}{path}"
+        headers = self.__authenticated_headers(headers)
+        return requests.post(url, headers=headers, **kwargs)
+    
+    def put(self, path="", headers={}, **kwargs):
+        url = f"{self.__api_root()}{path}"
+        headers = self.__authenticated_headers(headers)
+        return requests.put(url, headers=headers, **kwargs)
+    
+    def patch(self, path="", headers={}, **kwargs):
+        url = f"{self.__api_root()}{path}"
+        headers = self.__authenticated_headers(headers)
+        return requests.patch(url, headers=headers, **kwargs)
+    
+    def delete(self, path="", headers={}, **kwargs):
+        url = f"{self.__api_root()}{path}"
+        headers = self.__authenticated_headers(headers)
+        return requests.delete(url, headers=headers, **kwargs)
+
+class AstraDocuments:
+    def __init__(self, client, keyspace):
+        self.client = client
+        self.keyspace = keyspace
+    
+    def create(self, collection, document=dict(), id=None):    
+        path = f"/v2/namespaces/{self.keyspace}/collections/{collection}"
+        resp = self.client.post(path, data=json.dumps(document))
         if resp.status_code == requests.codes.created:
             return resp.json()
         else:
-            return resp.status_code
+            raise RuntimeError(f"{resp.status_code} response received. Expected 201")
--- backend/scratch.py
+++ backend/scratch.py
@@ -1,12 +1,5 @@
 import os
-from astra import Astra
+from astra import AstraClient
 
-db = os.environ['ASTRA_DATABASE_ID']
-region = os.environ['ASTRA_REGION']
-username = os.environ['ASTRA_DATABASE_USERNAME']
-password = os.environ['ASTRA_DATABASE_PASSWORD']
-keyspace = os.environ['ASTRA_KEYSPACE']
-
-a = Astra(db, region, username, password, keyspace)
-d = a.create_document("bar", {'foo': "bar"})
-
+a = AstraClient.from_environment()
+d = a.create("widgets", {'foo': "bar"})
