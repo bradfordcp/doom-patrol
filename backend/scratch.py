6fa2f6cd08be539b097935d81ae37a3b510dc474
Christopher Bradford <christopher.bradford@datastax.com>
2020-09-12 00:04:11 -0400

    Added comparison tests for sync vs async http

--- backend/scratch.py
+++ backend/scratch.py
@@ -18,22 +18,36 @@ with AstraClient.new() as client:
     collection = 'widgets'
 
     document = {'a': 'a'}
-    doc_count = 5
+    document_id = '00489f2c-5283-4fe8-a4c2-e8691982f917'
+    doc_count = 10
     documents = [document for i in range(doc_count)]
 
     loop = asyncio.get_event_loop()
 
     path = f"/v2/namespaces/{keyspace}/collections/{collection}"
 
-    with report_time("requests", doc_count):
+    with report_time("requests POST", doc_count):
         for document in documents:
             client.request('POST', path, json=document)
 
-    with report_time("aiohttp", doc_count):
+    with report_time("aiohttp POST", doc_count):
         reqs = []
         resps = []
 
         for document in documents:
             reqs.append(client.async_request('POST', path, json=document))
+        
+        res2 = loop.run_until_complete(asyncio.gather(*reqs))
+    
+    with report_time("requests GET", doc_count):
+        for document in documents:
+            client.request('GET', f"{path}/{document_id}").json()
+
+    with report_time("aiohttp GET", doc_count):
+        reqs = []
+        resps = []
+
+        for document in documents:
+            reqs.append(client.async_request('GET', f"{path}/{document_id}"))
         
         res2 = loop.run_until_complete(asyncio.gather(*reqs))
